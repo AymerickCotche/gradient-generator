184a7128d5bd446caaa6f14181b45afe7f01b0b0
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-03-17 19:58:28 +0100

    optimizing selector getCOlors

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,7 +1,7 @@
 import { RootState } from 'src/store';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getColors = (state: RootState) => ({
-  firstColor: state.firstColor,
-  lastColor: state.lastColor,
+export const getColors = ({ firstColor, lastColor }) => ({
+  firstColor,
+  lastColor,
 });
