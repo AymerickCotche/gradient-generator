88644c89cfc5e2ffba28b80170fbc15e99897f74
AymerickCotche <aymerick.cotche@hotmail.fr>
2022-03-17 20:17:17 +0100

    Reading store for Gradient coponent

--- src/components/Gradient/index.tsx
+++ src/components/Gradient/index.tsx
@@ -1,16 +1,18 @@
 // == Import
 import * as React from 'react';
 import { useAppSelector } from 'src/hooks';
+import { getColors } from 'src/selectors';
 import './styles.scss';
 
 // == Composant
 const Gradient = () => {
   const direction = useAppSelector((state) => state.direction);
+  const { firstColor, lastColor } = useAppSelector(getColors);
   return (
     <div
       id="gradient"
       style={{
-        background: `linear-gradient(${direction},#e367a4,#48b1f3)`,
+        background: `linear-gradient(${direction},${firstColor},${lastColor})`,
       }}
     />
   );
